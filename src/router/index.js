d1ed8d2fe760ed0eee8c10d5f76f0236799fa6c2
Khac Thanh <thanhcqb2048@gmail.com>
2025-03-18 01:32:14 +0700

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import DefaultLayout from '@/layouts/DefaultLayout.vue'
 import AboutView from '@/views/AboutView.vue'
@@ -15,7 +15,7 @@ import UserPostManage from '@/views/manage/UserPostManage.vue'
 import ChangePasswordView from '@/views/manage/ChangePasswordView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(),
   routes: [
     {
       path: '/',
