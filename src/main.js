2b1820504cedae1d5b968d358ceab1e0780d07f5
Khac Thanh <thanhcqb2048@gmail.com>
2025-03-19 02:17:59 +0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -18,4 +18,12 @@ app.use(Antd);
 app.use(createPinia())
 app.use(router)
 
+router.afterEach((to) => {
+  if (typeof window.gtag !== 'undefined') {
+    window.gtag('config', 'G-7W3SMN53WZ', {
+      'page_path': to.path,
+    });
+  }
+});
+
 app.mount('#app')
