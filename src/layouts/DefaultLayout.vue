4cede0246bbfa9c34cf2f8c3aa15eea221f3ec98
Khac Thanh <thanhcqb2048@gmail.com>
2025-03-25 11:04:33 +0700

    Update contact

--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -1,5 +1,13 @@
 <script setup>
 import Navbar from '@/components/Navbar.vue'
+import { FloatButton } from 'ant-design-vue';
+import { MessageOutlined } from '@ant-design/icons-vue';
+
+const goToFacebook = () => {
+  window.open('https://www.facebook.com/profile.php?id=61573035180828', '_blank');
+};
+
+
 </script>
 <template>
   <header>
@@ -11,5 +19,18 @@ import Navbar from '@/components/Navbar.vue'
       <RouterView />
     </main>
   </div>
+
+
+  <a-tooltip title="Liên hệ chúng tôi ngay">
+    <a-float-button
+      type="primary"
+      @click="goToFacebook"
+      style="right: 20px; bottom: 20px; background-color: #0084ff"
+    >
+      <template #icon>
+        <MessageOutlined />
+      </template>
+    </a-float-button>
+  </a-tooltip>
 
 </template>
